1be285f0329e7fd1fde05d2fb3f9cfc8454bad23
Albert Jojo <jojoalbert35@gmail.com>
2025-05-07 18:26:05 -0400

    Made a new branch for NSF_Access_Database(Bucket). Also split the scripts

--- postgres_operations.py
+++ postgres_operations.py
@@ -114,38 +114,6 @@ class PostgresDB:
             self.handle_error(e, "get_el_pairs")
             return {"error": str(e)}
 
-class NSF_DB:
-    def __init__(self,key_file):
-        """
-        Basic init function for the class NSF_DB,
-        need to check if NSF supports these parameters.
-        """
-        with open(key_file, 'r') as f:
-            keys = json.load(f)
-
-        self.access_key = keys['access_key']
-        self.secret_key = keys['secret_key']
-        #self.endpoint_url = keys['endpoint_url']
-
-        self.s3_client = boto3.client(
-                's3',
-                #endpoint_url = self.endpoint_url,
-                aws_access_key = self.access_key,
-                aws_secret_access = self.secret_key
-        )
-
-
-    def uri_generator(self,bucket_name:str):
-        pass
-
-    def upload_files(self, bucket_name:str)->None:
-        pass
-
-    def download_files(self,bucket_name:str)->None:
-        pass
-
-    def search_files(self,bucket_name:str)->bool:
-        pass
 
 
 # Example usage:
