f6086a7f58b7bd81acd0636078ae6b053edd3751
Albert Jojo <jojoalbert35@gmail.com>
2025-05-09 12:54:03 -0400

    Made some changes to the initialization NSF_DB

--- nsf_operations.py
+++ nsf_operations.py
@@ -11,25 +11,32 @@ Author: Albert
 
 import json
 import boto3
+from botocore.client import Config
 
 class NSF_DB:
     def __init__(self,key_file):
         """
-        Basic init function for the class NSF_DB,
-        need to check if NSF supports these parameters.
+        Initialize NSF_DB connection using credentials from key_file.
+        key_file should be a JSON file with:
+            {
+                "access_key": "YOUR_ACCESS_KEY",
+                "secret_key": "YOUR_SECRET_KEY",
+                "endpoint_url": "https://YOUR_OSN_ENDPOINT"
+            }
         """
         with open(key_file, 'r') as f:
             keys = json.load(f)
 
         self.access_key = keys['access_key']
         self.secret_key = keys['secret_key']
-        #self.endpoint_url = keys['endpoint_url']
+        self.endpoint_url = keys['endpoint_url']
 
         self.s3_client = boto3.client(
                 's3',
-                #endpoint_url = self.endpoint_url,
+                endpoint_url = self.endpoint_url,
                 aws_access_key = self.access_key,
-                aws_secret_access = self.secret_key
+                aws_secret_access = self.secret_key,
+                config=Config(signature_version='s3v4')
         )
 
 
