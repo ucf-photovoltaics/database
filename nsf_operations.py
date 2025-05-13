8eec8a549762a0c2bf4a083a13d8270dcf2adbe5
Albert Jojo <jojoalbert35@gmail.com>
2025-05-13 16:42:58 -0400

    Added the presigned_url and upload files functions.

--- nsf_operations.py
+++ nsf_operations.py
@@ -12,7 +12,8 @@ import json
 import boto3
 import pandas as pd
 import os
-from botocore.client import Config
+from botocore.client import Config, ClientError
+
 
 class NSF_DB:
     def __init__(self,key_file):
@@ -40,19 +41,34 @@ class NSF_DB:
                 aws_secret_access = self.secret_key,
                 config=Config(signature_version='s3v4')
         )
+    
+    # No need to create URI's manually use presigned_uri's since the bucket is private
+    def generate_presigned_url(self, bucket_name, object_key, expiration=3600):
+        try:
+            return self.s3_client.generate_presigned_url(
+                'get_object',
+                Params={'Bucket':bucket_name, 'key':object_key},
+                ExpiresIn=expiration
+            )
+        except ClientError as e:
+            print(f"Failed to generate presigned URL for {object_key}: {e}")
+        
 
+    def upload_files(self, input_files:pd.Series, bucket_name:str)->None:
 
-    def uri_generator(self, bucket_name:str):
-        pass
+        for file_path in input_files.to_list():
+            file_name = os.path.basename(file_path)
 
-    def upload_files(self, input:DataFrame.Series,bucket_name:str)->None:
+            try:
+                self.s3_client.upload_file(file_path, bucket_name, file_name)
+                print(f"Upload of {file_name} succesfull")
+            except ClientError as e:
+                print(f"Error uploading {file_name}:{e}")
+
+    def download_files(self, bucket_name:str, output: pd.Series)->None:
         """Column from a dataframe (Series)"""
         pass
 
-    def download_files(self, bucket_name:str, output: DataFrame.Series)->None:
-        """Column from a dataframe (Series)"""
-        pass
-
-    """Source -> destination, can be buckets for both param, input==> path, (globus)"""
+    """Source -> destination, can be buckets for both param, input==> path, try to implement (globus)"""
     def transfer_files(source, dest):
         pass
